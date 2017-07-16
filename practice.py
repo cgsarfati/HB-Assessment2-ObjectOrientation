aa8779a949c8bf093efee959c1f1445f28437259
vagrant <vagrant@vagrant.vm>
2017-07-16 19:47:41 +0000

    finished part 4; exercise complete. removed doctest files.

--- practice.py
+++ practice.py
@@ -27,27 +27,93 @@ class Question(object):
     def ask_and_evaluate(self):
         """ """
 
-        user_answer = raw_input(self.question + " > ")
+        user_answer = raw_input(self.question + " ")
 
-        if user_answer == self.correct_answer:
-            return True
-        else:
-            return False
+        return user_answer == self.correct_answer
 
 
 class Exam(object):
     """ Creates an exam. """
 
-    def __init__(self, name, questions=[]):
+    def __init__(self, name):
         self.name = name.lower()
         self.questions = []
+        self.score = None
 
-    def add_question(self, question):
+    def add_question(self, question_set):
         """ """
-        self.questions.append(question)
+
+        self.questions.append(question_set)
 
     def administer(self):
         """ """
 
-        for question in self.questions():
-            ask_and_evaluate()
+        correct_answers_tally = 0
+
+        for question in self.questions:
+            if Question.ask_and_evaluate(question):
+                correct_answers_tally += 1
+
+        return float(correct_answers_tally) / len(self.questions)
+
+
+class StudentExam(object):
+    """ """
+
+    def __init__(self, student, exam):
+        """ """
+        self.student = student
+        self.exam = exam
+        self.score = None
+
+    def take_test(self):
+        """ """
+        self.score = Exam.administer()
+        print self.score
+
+
+def example():
+    """ """
+
+    current_exam = Exam("Midterm")
+
+    set_A = Question("Is Balloonicorn a support staff?", "Yes")
+    set_B = Question("What color is Balloonicorn?", "Pink")
+    set_C = Question("What floor does Balloonicorn live?", "2nd")
+
+    current_exam.add_question(set_A)
+    current_exam.add_question(set_B)
+    current_exam.add_question(set_C)
+
+    current_student = Student("Charlotte", "Sarfati", "1200 4th st")
+
+    current_student_exam = StudentExam(current_student, current_exam)
+    current_student_exam.take_test()
+
+
+class Quiz(Exam):
+    """ """
+
+    def administer(self):
+        """ """
+
+        self.score = super(Quiz, self).administer()
+
+        if self.score >= 0.5:
+            return 1
+        else:
+            return 0
+
+
+class StudentQuiz(object):
+    """ """
+
+    score = 0
+
+    def __init__(self, student, quiz):
+        self.student = student
+        self.quiz = quiz
+
+    def take_test(self, student, quiz):
+        self.score = Quiz.administer()
+        print self.score
